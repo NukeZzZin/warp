92ebf6eac7c3c4fc552b2c6750a62ad1d5a5fd14
NukeZzZin <nukezzzin@gmail.com>
2022-11-05 17:41:38 -0300

    implemented a 'glfw' to 'main.cpp'

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <glfw/glfw3.h>
 
-int main(int arc, char** argv)
+int main(int argc, char** argv)
 {
-	return EXIT_SUCCESS;
+    if (!glfwInit())
+    {
+        std::cerr << "failed to initialize 'GLFW'." << std::endl;
+        return EXIT_FAILURE;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Window", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cerr << "failed to initialize 'GLFW' Window." << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwSwapInterval(1);
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
